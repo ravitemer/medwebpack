915e361cd4ef3d21e148158f6cc0da7e688a38cc
play.js <git@gituser.com>
2021-06-28 00:34:38 +0530

    listening Part A input updated

--- src/components/TestParts/lisPartA.svelte
+++ src/components/TestParts/lisPartA.svelte
@@ -10,17 +10,22 @@ export let desc;
  <Block>{@html desc}</Block>
  <BlockTitle medium>Extract 1 : Q1-12</BlockTitle>
  <Block>
-   <BlockHeader>{extract1.desc}</BlockHeader>
+   <BlockHeader>{@html extract1.desc}</BlockHeader>
    {#each extract1.headings as [subheading,content],i (i)}
-      <h3 class="text-color-blue">{subheading}</h3>
-      {@html content.para}
+      <h3 class="text-color-teal">{subheading}</h3>
+      <p style="font-size:1.3em">
+     {@html content.para.replaceAll(/{[^{].*}/g, '<input  class="text-color-green" style=";border-radius:2px;border-bottom:1px solid gray;width:120px;padding:3px;display:inline" type="text"/>')}
+      </p>
     {/each}
  </Block>
  <BlockTitle medium>Extract 2 : Q13-24</BlockTitle>
  <Block>
-    <BlockHeader>{extract2.desc}</BlockHeader>
+    <BlockHeader>{@html extract2.desc}</BlockHeader>
     {#each extract2.headings as [subheading,content],i (i)}
-      <h3 class="text-color-blue">{subheading}</h3>
-      {@html content.para}
+    
+      <h3 class="text-color-teal">{subheading}</h3>
+      <p style="font-size:1.3em">
+      {@html content.para.replaceAll(/{[^{].*}/g, '<input  class="text-color-green" style="color:green;border-radius:2px;border-bottom:1px solid gray;width:120px;padding:3px;display:inline" type="text"/>')}
+      </p>
     {/each}
 </Block>
